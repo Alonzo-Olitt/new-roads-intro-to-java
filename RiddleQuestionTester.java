be8c3fe4954f11184f584ee156d6801724e4be4a
aolitt <aolitt@newroads.org>
2017-01-18 12:20:21 -0500

    RiddleQuestion.java

---
+++ RiddleQuestionTester.java
@@ -0,0 +1,10 @@
+public class RiddleQuestionTester{
+  
+  public static void main(String[] args){
+    
+    RiddleQuestion riddlequestion = new RiddleQuestion("what is brown and sticky?", "A stick");
+    System.out.println(riddlequestion);
+  }
+  
+  
+}
