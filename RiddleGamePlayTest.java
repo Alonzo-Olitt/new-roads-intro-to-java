dcf30fd1e77e8561c7f79f7102445d98b74adcf1
aolitt <aolitt@newroads.org>
2017-02-08 12:28:47 -0500

    RiddleGamePlayTest.java

--- RiddleGamePlayTest.java
+++ RiddleGamePlayTest.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 public class RiddleGamePlayTest{
   
+      public static int getRandomNumber(int max, int min){
+        int rangeX = (max - min) + 1;
+        
+        return (int)(Math.random() * rangeX) + min;
+      }
+  
   public static void main(String[] args){
     
     String name;
     String answer;
     int riddleIndex = 0;
     RiddleQuestion activeRiddle;
+    
+    
     //put all riddles in array
     RiddleQuestion[] riddles = 
     {
      new RiddleQuestion("What is brown and sticky?", "stick"),
      new RiddleQuestion("What is heavy forward and not backwards?", "ton"),
-     new RiddleQuestion("Gianni's father has three sons : SJ, Tyji, and ", "Gianni")
+     new RiddleQuestion("Gianni's father has three sons : SJ, Tyji, and ", "Gianni"),
+     new RiddleQuestion("What part of the turkey has the most feathers?", "outside"),
+     new RiddleQuestion("I go in hard. I come out soft. You blow me hard. What am I?", "gum"),
+     new RiddleQuestion("What begins with T, ends with T and has T in it?", "teapot")
     };
     
     
@@ -33,7 +44,7 @@ public class RiddleGamePlayTest{
     //while user says yes to 
     while(answer.equals("yes")){
       
-      activeRiddle = riddles[riddleIndex];
+      activeRiddle = riddles[getRandomNumber(riddles.length-1,0)];
     
       //ask riddle 
       System.out.println(activeRiddle.getQuestion());
